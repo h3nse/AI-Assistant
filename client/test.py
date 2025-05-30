0dc988f22faad22ab8c0f07b9f477f9ee7e8290b
h3nse <henrikf.bj@gmail.com>
2025-05-30 18:08:02 +0200

    Reorganize

--- client/test.py
+++ client/test.py
@@ -1,7 +1,13 @@
 import requests
 
+res = requests.get(
+    "https://cloud-server-testing-production.up.railway.app/get",
+)
+print(res.json())
+
 res = requests.post(
     "https://cloud-server-testing-production.up.railway.app/post",
     json={"TestKey": "Hello"},
 )
+
 print(res.json())
